d364c89af4cf1f38398fd1799f8144d3575b49e6
Niall Fraser <niallfraser92@gmail.com>
2019-04-22 15:04:22 -0500

    commit before pulling master

---
+++ src/Models/Facilities/NaturalField.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Trestlebridge.Interfaces;
+using Trestlebridge.Models.Animals;
+using Trestlebridge.Actions;
+
+
+namespace Trestlebridge.Models.Facilities {
+    public class GrazingField : IFacility<IGrazing>
+    {
+        private int _capacity = 20;
+        private Guid _id = Guid.NewGuid();
+
+        private List<IGrazing> _animals = new List<IGrazing>();
+
+        public double Capacity {
+            get {
+                return _capacity;
+            }
+        }
+        public List<IGrazing> animalsList {
+            get {
+                return _animals;
+            }
+        }
+
+        public void AddResource (IGrazing animal, Farm farm)
+        {
+            if (_animals.Count < _capacity) {
+                _animals.Add(animal);
+            }
+            else {
+                ChooseGrazingField.atCapacity = true;
+                ChooseGrazingField.CollectInput(farm, animal);
+            }
+        }
+
+         public void AddResource (IGrazing animal)
+        {
+            if (_animals.Count < _capacity) {
+                _animals.Add(animal);
+            }
+        }
+
+        public void AddResource (List<IGrazing> animals)  // TODO: Take out this method for boilerplate
+        {
+            if (_animals.Count + animals.Count <= _capacity) {
+                _animals.AddRange(animals);
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
+
+            output.Append($"Grazing field {shortId} has {this._animals.Count} animals\n");
+            this._animals.ForEach(a => output.Append($"   {a}\n"));
+
+            return output.ToString();
+        }
+    }
+}
