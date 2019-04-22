fa5680f9836cdea3ed057738ddf5b28014008034
Warner Carpenter <warnercarpenter@yahoo.com>
2019-04-22 14:36:46 -0500

    made sunflower and wildflower classes. Made ICompostProducing interface

---
+++ src/Models/Plants/Sunflower.cs
@@ -0,0 +1,23 @@
+using System;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Plants
+{
+    public class Sunflower : IResource, ISeedProducing, ICompostProducing
+    {
+        private int _seedsProduced = 650;
+        private double _compostProduced = 21.6;
+        public string Type { get; } = "Sunflower";
+
+        public double Harvest () {
+            return _seedsProduced;
+        }
+        public double Compost () {
+            return _compostProduced;
+        }
+
+        public override string ToString () {
+            return $"Sunflower. Oh boy!";
+        }
+    }
+}
