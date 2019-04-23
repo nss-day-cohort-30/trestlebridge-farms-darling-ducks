d7d511f41b2f08cd33b882d51e34f7bb7164bd6b
Emmett Schneider <emmett.f.schneider@gmail.com>
2019-04-23 10:13:15 -0500

    added PurchaseSeed.cs; still need to be able to put purchased seeds in a facility

--- src/Actions/PurchaseSeed.cs
+++ src/Actions/PurchaseSeed.cs
@@ -4,27 +4,37 @@ using Trestlebridge.Models;
 using Trestlebridge.Models.Plants;
 using Trestlebridge.Models.Facilities;
 
-namespace Trestlebridge.Actions {
-    public class PurchaseSeed {
-        public static void CollectInput (Farm farm) {
-            Console.WriteLine ("1. Sesame");
-            Console.WriteLine ("2. Sunflower");
-            Console.WriteLine ("3. Wildflower");
+namespace Trestlebridge.Actions
+{
+  public class PurchaseSeed
+  {
+    public static void CollectInput(Farm farm)
+    {
+      Console.WriteLine("1. Sesame");
+      Console.WriteLine("2. Sunflower");
+      Console.WriteLine("3. Wildflower");
 
-            Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+      Console.WriteLine();
+      Console.WriteLine("What are you buying today?");
 
-            Console.Write ("> ");
-            string choice = Console.ReadLine ();
+      Console.Write("> ");
+      string choice = Console.ReadLine();
 
-            switch (Int32.Parse(choice))
-            {
-                case 1:
-                    ChoosePlowedField.CollectInput(farm, new Sesame());
-                    break;
-                default:
-                    break;
-            }
-        }
+      switch (Int32.Parse(choice))
+      {
+        case 1:
+          ChoosePlowedField.CollectInput(farm, new Sesame());
+          break;
+        case 2:
+          ChoosePlowedField.CollectInput(farm, new Sunflower());
+          ChooseNaturalField.CollectInput(farm, new Sunflower());
+          break;
+        case 3:
+          ChooseNaturalField.CollectInput(farm, new Wildflower());
+          break;
+        default:
+          break;
+      }
     }
+  }
 }
