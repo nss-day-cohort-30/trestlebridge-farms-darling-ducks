a562598947ebbb5b830ae8cb0eefbe7e12d13b48
Mo Silvera <mo.leslie.silvera@gmail.com>
2019-04-23 09:36:56 -0500

    added the create facility logic

--- src/Actions/CreateFacility.cs
+++ src/Actions/CreateFacility.cs
@@ -8,6 +8,9 @@ namespace Trestlebridge.Actions {
         public static void CollectInput (Farm farm) {
             Console.WriteLine ("1. Grazing field");
             Console.WriteLine ("2. Plowed field");
+            Console.WriteLine ("3. Natural field");
+            Console.WriteLine ("4. Chicken House");
+            Console.WriteLine ("5. Duck House");
 
             Console.WriteLine ();
             Console.WriteLine ("Choose what you want to create");
@@ -20,8 +23,20 @@ namespace Trestlebridge.Actions {
                 case 1:
                     farm.AddGrazingField(new GrazingField());
                     break;
-                default:
+                case 2:
+                    farm.AddPlowedField(new PlowedField());
                     break;
+                case 3:
+                    farm.AddNaturalField(new NaturalField());
+                    break;
+                case 4:
+                    farm.AddChickenHouse(new ChickenHouse());
+                    break;
+                case 5: AddDuckHouse (new DuckHouse());
+                    break;
+                    default:
+                    break;
+
             }
         }
     }
