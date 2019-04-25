f5ababfb859d0f26f45ad4caf22d67a545a5d184
Warner Carpenter <warnercarpenter@yahoo.com>
2019-04-25 14:56:34 -0500

    conmpost producer checks for goats in grazing fields

--- src/Actions/ChooseComposter.cs
+++ src/Actions/ChooseComposter.cs
@@ -6,34 +6,45 @@ using Trestlebridge.Models.Plants;
 using Trestlebridge.Models.Facilities;
 using System.Collections.Generic;
 
-namespace Trestlebridge.Actions {
-    public class ChooseComposter {
+namespace Trestlebridge.Actions
+{
+    public class ChooseComposter
+    {
 
-        public static void CollectInput (Farm farm) {
+        public static void CollectInput(Farm farm)
+        {
             Console.Clear();
 
             for (int i = 0; i < farm.NaturalFields.Count; i++)
-                {
-                        Console.WriteLine($"{i + 1}. Natural Field ({farm.NaturalFields[i].plantsList.Count}) plants");
-                }
+            {
+                Console.WriteLine($"{i + 1}. Natural Field ({farm.NaturalFields[i].plantsList.Count} plant(s))");
+            }
 
-                for (int i = 0; i < farm.PlowedFields.Count; i++)
-                {
-                         IEnumerable<ISeedProducing> sunflowersInPlowedField = from plant in farm.PlowedFields[i].plantsList
-                                where plant.Type == "Sunflower"
-                                select plant;
+            for (int i = 0; i < farm.PlowedFields.Count; i++)
+            {
+                IEnumerable<ISeedProducing> sunflowersInPlowedField = from plant in farm.PlowedFields[i].plantsList
+                                                                      where plant.Type == "Sunflower"
+                                                                      select plant;
 
-                        Console.WriteLine($"{farm.NaturalFields.Count + i + 1}. Plowed Field ({sunflowersInPlowedField.Count()}) plants");
+                Console.WriteLine($"{farm.NaturalFields.Count + i + 1}. Plowed Field ({sunflowersInPlowedField.Count()} plant(s))");
+            }
 
-                }
+            for (int i = 0; i < farm.GrazingFields.Count; i++)
+            {
+                IEnumerable<IGrazing> goatsInGrazingField = from animal in farm.GrazingFields[i].animalsList
+                                                                      where animal.Type == "Goat"
+                                                                      select animal;
 
-                Console.WriteLine();
+                Console.WriteLine($"{farm.NaturalFields.Count + farm.PlowedFields.Count + i + 1}. Grazing Field ({goatsInGrazingField.Count()} goat(s))");
+            }
 
-                // How can I output the type of plant chosen here?
-                Console.WriteLine($"Which Facility would you like to process");
+            Console.WriteLine();
 
-                Console.Write("> ");
-                Console.ReadLine();
+            // How can I output the type of plant chosen here?
+            Console.WriteLine($"Which Facility would you like to process");
+
+            Console.Write("> ");
+            Console.ReadLine();
             //     int choice = Int32.Parse(Console.ReadLine());
             //     int correctedChoice = choice - 1;
 
